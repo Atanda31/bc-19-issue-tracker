63cc1eb4aa0f260e67708e507d5f778c9471cd44
Atanda <microsem31@gmail.com>
2016-08-25 11:54:07 +0100

    Removal of redundant ejs file. Also User registration fix with Firebase and adding script to register.ejs.

---
+++ views/js/register.js
@@ -0,0 +1,38 @@
+(function() {
+
+	var config = {
+
+		apiKey: "AIzaSyAOgI44K8i_fEIAMfMI0NXBFxRSWL5KYYE",
+		authDomain: "issue-tracker-371c3.firebaseapp.com",
+		databaseURL: "https://issue-tracker-371c3.firebaseio.com",
+		storageBucket: "issue-tracker-371c3.appspot.com",
+		};
+
+
+		firebase.initializeApp(config);
+				  
+		var register = document.getElementById('register');
+
+		var app = firebase.database().ref(),
+	  	clients = app.child('users');
+	  	var auth = firebase.auth();
+        register.addEventListener('click', e => {
+        	var email = document.getElementById('email').value,
+        	    password = document.getElementById('password').value
+        	e.preventDefault();
+        	auth.createUserWithEmailAndPassword(email, password).then((user)=>{
+            console.log(user);
+            console.log(user.uid);
+            alert("Your profile has been succesfully created & You are now Signed In");
+            window.location.href = "/home";
+        }).catch((e)=>{
+            console.log(err);
+        });
+
+	        clients.push({
+				name: document.getElementById('name').value,
+				username: document.getElementById('username').value
+			});
+	        });
+	  	 
+}())
